216ea9397b39ae90d2d4082198b598e4a35e0bf0
donghuizhou <765966797@qq.com>
2021-12-11 19:40:08 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import _ from "lodash";
+import printMe from "./print";
+
+function component () {
+    const element = document.createElement("div");
+    const btn = document.createElement("button");
+
+    // lodash 在当前 script 中使用 import 引入
+    element.innerHTML = _.join(["Hello", "webpack11"], " ");
+
+    btn.innerHTML = "click me and check the console";
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    return element;
+}
+
+document.body.appendChild(component());
